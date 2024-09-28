78206d045da9291da6ebf428f67b16f09007b966
yamin <yamin148@infinity.gmail.com>
2024-09-28 16:28:48 +0700

    header and search in side bar

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,5 +1,8 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { useEffect, useRef } from 'react';
 import styles from '../styles/SideBar.module.css';
+import Image from 'next/image';
+import { RiSearchLine } from 'react-icons/ri';
 
 export default function SideBar({ setIsSiebar, isSiebar, width }) {
   const sideBarRef = useRef(null);
@@ -30,7 +33,45 @@ export default function SideBar({ setIsSiebar, isSiebar, width }) {
       className={`${styles.wrapper} ${ isSiebar ? 'w-[250px]' : 'w-0 sm:w-[70px]' }`}
       ref={sideBarRef}
     >
-      <h1>SideBar</h1>
+      <div
+        className={styles.header}
+      >
+        <Image 
+          src={'/logo.png?version=3'}
+          width={35}
+          height={35}
+        />
+        <h1 
+          className={styles.title}
+          onClick={() => alert('hello world')}
+        >Admin Site</h1>
+      </div>
+      <div
+        className={styles.personal}
+      >
+        <Image 
+          src={'/person.png'}
+          width={35}
+          height={35}
+        />
+        <h1 
+          className={styles['account-user']}
+          onClick={() => alert('hello world')}
+        >Alexander Pierce</h1>
+      </div>
+      <div
+        className={styles.search}
+      >
+        <input 
+          type="text" 
+          placeholder='Search' 
+          className={styles['search-input']}
+        />
+        <RiSearchLine 
+          className={styles['search-btn']} 
+          onClick={() => alert('Searching')}
+        />
+      </div>
     </section>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: '#23426f',
+        secondary: '#486185',
       },
     },
   },
